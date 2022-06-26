75d2fb8faf8837fe7ce5e54f249dab688b3795fd
Aneokaos <107446035+Aneokaos@users.noreply.github.com>
2022-06-26 14:16:14 -0500

    Se agrega PickerView

--- Panka/ViewController.swift
+++ Panka/ViewController.swift
@@ -8,12 +8,86 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var CitySourceTextField: UITextField!
+    @IBOutlet weak var CityDestinyTextField: UITextField!
+    
+    
+    let citys = ["Lima","Piura","Chiclayo","Arequipa","Pucallpa"]
+    let regiones = ["Costa","Sierra","Selva"]
+    
+    
+    var CitySourcePickerView = UIPickerView ()
+    var CityDestinyPickerView = UIPickerView ()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        CitySourceTextField.inputView = CitySourcePickerView
+        CityDestinyTextField.inputView = CityDestinyPickerView
+        
+        CitySourceTextField.placeholder = "Selecciona Ciudad"
+        CityDestinyTextField.placeholder = "Selecciona Región"
+        
+        CitySourceTextField.textAlignment = .center
+        CityDestinyTextField.textAlignment = .center
+        
+        
+        CitySourcePickerView.delegate = self
+        CitySourcePickerView.dataSource = self
+        CityDestinyPickerView.delegate = self
+        CityDestinyPickerView.dataSource = self
+        
+        
+        CitySourcePickerView.tag = 1
+        CityDestinyPickerView.tag = 2
+        
+        
     }
-
-
 }
+
+
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        switch pickerView.tag {
+        case 1:
+            return citys.count
+        case 2:
+            return regiones.count
+        default:
+            return 1
+        }
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch pickerView.tag {
+        case 1:
+            return citys[row]
+        case 2:
+            return regiones[row]
+        default:
+            return "Data not found"
+        }
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+     switch pickerView.tag {
+        case 1:
+         CitySourceTextField.text = citys [row]
+         CitySourceTextField.resignFirstResponder()
+        case 2:
+         CityDestinyTextField.text = regiones [row]
+         CityDestinyTextField.resignFirstResponder()
+        default:
+            return
+        }
+        
+    }
+    }
+    
 
